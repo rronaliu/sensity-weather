a1c4cb1c694a644d499a0769b4851ee4bbeb218b
Rron Aliu <rron.a@gjirafa.com>
2022-10-17 21:47:55 +0200

    [chore] change stuff that i added for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sensity-weather/",
+  // this was a line to add so I could host it on gh-pages
+  // base: "/sensity-weather/",
   plugins: [vue()],
 });
